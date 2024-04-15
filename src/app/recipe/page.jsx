3195ecabb09123685961e51e9f1924e3f405c4c5
raul.garciad <raul.garciad@sngular.com>
2024-04-15 13:14:13 +0200

    start cooking component

--- src/app/recipe/page.jsx
+++ src/app/recipe/page.jsx
@@ -1,41 +1,14 @@
-'use client';
-
 import IconComp from "../components/iconComp";
 import InfoComp from "../components/infoComp";
-import {useParams} from "next/navigation";
-import {useEffect} from "react";
-//import {useRecipeStore} from "../../hooks/storeHooks/useRecipeStore";
-//import {useParams} from "react-router-dom";
+import StartCooking from "../components/startCooking";
+
 
 function Recipe() {
-    const [seeStartCooking, setSeeStartCooking] = useState(false);
-  //  const {ingredients, steps, name, cookingTime, servings, getById} = useRecipeStore();
     const ingredients = []
     const steps = []
     const name = "Ensalada de lechuga"
     const cookingTime = "10 minutos"
     const servings = 4
-    //get recipeId from url
-    //const {id: recipeId} = useParams();
-    useEffect(() => {
-        const handleScroll = () => {
-            setSeeStartCooking(window.scrollY >= 1);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        // Limpiar el event listener cuando el componente se desmonta
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-    useEffect(() => {
-        if (document.documentElement.scrollHeight <= window.innerHeight) {
-            setSeeStartCooking(true);
-        } else {
-            setSeeStartCooking(false);
-        }
-    }, [ingredients]);
 
     return (
         <>
@@ -102,16 +75,7 @@ function Recipe() {
                     </div>
                 </div>
             </div>
-            {seeStartCooking && (
-                <div
-                    id="start"
-                    className="h-16 w-11/12 bg-primary object-center rounded-full place-self-center flex gap-2 items-center justify-center fixed bottom-6 ml-auto mr-auto left-0 right-0"
-                >
-                    <IconComp icon="right-arrow-white" class="h-10 border-2 p-2 rounded-full"/>
-                    <p className="text-white font-extrabold">¿Listo? Comenzar a cocinar</p>
-                </div>
-            )}
-
+            <StartCooking/>
         </>
     );
 }
