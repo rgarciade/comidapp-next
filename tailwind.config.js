bec52c07d84f700075752525e487441915a31502
raul.garciad <raul.garciad@sngular.com>
2024-04-15 13:14:24 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,44 @@
+const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
         './src/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
         extend: {
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic':
-                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            fontSize: {
+                "4xl": "2.8rem",
+                "3xl": "1.8rem",
+                "2xl": "1.5rem"
+            },
+            textShadow: {
+                "2xl": "5px 10px;"
+            },
+            textColor: {
+                "primary": "#E49756",
+                "secondary": "#bb7e4b"
+            },
+            colors: {
+                "primary": "#E49756",
+                "secondary": "#bb7e4b",
+            },
+            backgroundColor: {
+                "primary": "#E49756",
+                "secondary": "#bb7e4b",
+            },
+            themeColor: {
+                "primary": "#f7f7f7"
             },
         },
     },
-    plugins: [],
+    plugins: [
+        plugin(function ({addBase, theme}) {
+            addBase({
+                "h1": {fontSize: theme("fontSize.4xl")},
+                "h2": {fontSize: theme("fontSize.3xl")},
+                "h3": {fontSize: theme("fontSize.2xl")}
+            });
+        })
+    ],
 }
