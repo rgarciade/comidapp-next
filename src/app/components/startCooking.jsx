cf38d9d8fe0a71463f37adf31816619f41ead7d8
raul.garciad <raul.garciad@sngular.com>
2024-04-16 18:14:27 +0200

    start cooking toggle

--- src/app/components/startCooking.jsx
+++ src/app/components/startCooking.jsx
@@ -7,7 +7,7 @@ function StartCooking () {
     const [seeStartCooking, setSeeStartCooking] = useState(false);
     useEffect(() => {
         const handleScroll = () => {
-            //setSeeStartCooking(window.scrollY >= 1);
+            setSeeStartCooking(window.scrollY >= 1);
         };
 
         window.addEventListener('scroll', handleScroll);
@@ -17,16 +17,17 @@ function StartCooking () {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
-    // useEffect(() => {
-    //     if (document.documentElement.scrollHeight <= window.innerHeight) {
-    //         setSeeStartCooking(true);
-    //     } else {
-    //         setSeeStartCooking(false);
-    //     }
-    // }, [ingredients]);
+    useEffect(() => {
+        if (document.documentElement.scrollHeight <= window.innerHeight) {
+            setSeeStartCooking(true);
+        } else {
+            setSeeStartCooking(false);
+        }
+    }, []);
     return (
         <>
 
+            {seeStartCooking && (
                 <div
                     id="start"
                     className="h-16 w-11/12 bg-primary object-center rounded-full place-self-center flex gap-2 items-center justify-center fixed bottom-6 ml-auto mr-auto left-0 right-0"
@@ -34,7 +35,7 @@ function StartCooking () {
                     <IconComp icon="right-arrow-white" class="h-10 border-2 p-2 rounded-full"/>
                     <p className="text-white font-extrabold">¿Listo? Comenzar a cocinar</p>
                 </div>
-
+            )}
         </>
     );
 }
